0815669c2badcb589223686ccf29ad8ebf3d0efd
ivanstanley143 <66038840+ivanstanley143@users.noreply.github.com>
2024-12-14 21:50:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,13 +14,37 @@ API_HASH = env.get('API_HASH', '')
 BOT_TOKEN = env.get('BOT_TOKEN', '')
 DB_URL = env.get('DB_URL', '')
 ADMINS = int(env.get('ADMINS', ''))
+AUTH_CHANNEL = int(env.get('AUTH_CHANNEL', ''))
 
 Dbclient = AsyncIOMotorClient(DB_URL)
 Cluster = Dbclient['Cluster0']
 Data = Cluster['users']
 Bot = Client(name='AutoAcceptBot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
        
-      
+async def get_fsub(bot, message):
+    target_channel_id = AUTH_CHANNEL  # Your channel ID
+    user_id = message.from_user.id
+    try:
+        # Check if user is a member of the required channel
+        await bot.get_chat_member(target_channel_id, user_id)
+    except UserNotParticipant:
+        # Generate the channel invite link
+        channel_link = (await bot.get_chat(target_channel_id)).invite_link
+        join_button = InlineKeyboardButton("🔔 Join Our Channel", url=channel_link)
+
+        # Display a message encouraging the user to join
+        keyboard = [[join_button]]
+        await message.reply(
+            f"<b>👋 Hello {message.from_user.mention()}, Welcome!</b>\n\n"
+            "📢 <b>Exclusive Access Alert!</b> ✨\n\n"
+            "To unlock all the amazing features I offer, please join our updates channel. "
+            "This helps us keep you informed and ensures top-notch service just for you! 😊\n\n"
+            "<i>🚀 Join now and dive into a world of knowledge and creativity!</i>",
+            reply_markup=InlineKeyboardMarkup(keyboard),
+        )
+        return False
+    else:
+        return True      
      
 @Bot.on_message(filters.command("start") & filters.private)                    
 async def start_handler(c, m):
