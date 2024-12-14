730b969b6053f732c559a34385fb33b12886c36d
ivanstanley143 <66038840+ivanstanley143@users.noreply.github.com>
2024-12-14 21:21:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,11 +9,11 @@ import asyncio, datetime, time
 ACCEPTED_TEXT = "Hey {user}\n\nYour Request For {chat} Is Accepted ✅"
 START_TEXT = "Hai {}\n\nI am Auto Request Accept Bot With Working For All Channel. Add Me In Your Channel To Use"
 
-API_ID = int(env.get('API_ID'))
-API_HASH = env.get('API_HASH')
-BOT_TOKEN = env.get('BOT_TOKEN')
-DB_URL = env.get('DB_URL')
-ADMINS = int(env.get('ADMINS'))
+API_ID = int(env.get('API_ID', ''))
+API_HASH = env.get('API_HASH', '')
+BOT_TOKEN = env.get('BOT_TOKEN', '')
+DB_URL = env.get('DB_URL', '')
+ADMINS = int(env.get('ADMINS', ''))
 
 Dbclient = AsyncIOMotorClient(DB_URL)
 Cluster = Dbclient['Cluster0']
